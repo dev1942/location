cebbc28d2edad0e92f57b94e5d2c77e74ff8ee83
ahmed <cozmoz.qa@gmail.com>
2022-05-24 03:12:19 +0500

    now

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import 'package:get/get.dart';
 
@@ -25,10 +24,10 @@ class HomeView extends GetView<HomeController> {
       // _scaffoldKey.currentState.show
       print(e);
       print('something horrible wrong happened');
-      var snackbar = SnackBar(
-        content: Text(e.toString()),
-        duration: Duration(milliseconds: 300),
-      );
+      // var snackbar = SnackBar(
+      //   content: Text(e.toString()),
+      //   duration: Duration(milliseconds: 300),
+      // );
       // GlobalKey<ScaffoldState>().currentState.ScaffoldMessenger.showSnackBar(snackbar);
 
       // GlobalKey<ScaffoldState>().showSnackBar( SnackBar( content: Text("Incremented"), duration: Duration(milliseconds: 300), ), );
@@ -41,68 +40,85 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Colors.blue,
-      body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.location_on,
-              color: Colors.yellow,
-              size: 40,
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            FloatingActionButton(
-              onPressed: postData,
-              child: Text('post Ypur Data'),
-            ),
-            Text(
-              'User Location',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  decoration: TextDecoration.underline),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Obx(
-              () => Text(
-                controller.latitude.value,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            Obx(
-              () => Text(
-                controller.longitude.value,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            Obx(
-              () => Text(
-                controller.address.value,
-                style: TextStyle(color: Colors.black, fontSize: 25),
-              ),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 238, 220, 64),
         ),
-      )),
+        child: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
+                color: Color.fromARGB(255, 14, 12, 1),
+                size: 40,
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                'User Location',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    decoration: TextDecoration.underline),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Obx(
+                () => Text(
+                  controller.latitude.value,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Obx(
+                () => Text(
+                  controller.longitude.value,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Obx(
+                () => Text(
+                  controller.address.value,
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                //style: ButtonStyle(backgroundColor: Color.fromARGB(255, 21, 98, 160),),
+                onPressed: postData,
+                child: Text(
+                  'Post Your Location to our hackers',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    height: 1.5,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
